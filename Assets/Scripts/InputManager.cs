1a7544ff8dba6ecda5babda6a708c6346d38ad96
eviltwo <e135764@DaikiMacBookPro.local>
2014-08-24 12:12:38 +0900

    InputManagerの作成
    クリック・継続クリック・クリック位置の取得が可能になった。

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 入力情報を取得する
+/// </summary>
+public class InputManager : SingletonMonoBehaviour<InputManager> {
+
+	int ClickLevel = 0;						// クリック状態 0:無し 1:初クリック 2:継続(ドラッグの可能性あり)
+	Vector2 LastClickPos = Vector2.zero;	// 最後にクリックした位置
+
+
+	// Update is called once per frame
+	void Update (){
+		if (Application.platform == RuntimePlatform.Android) {
+			checkTouch ();		// スマホクリック判定
+		} else {
+			checkClick ();		// PCクリック判定
+		}
+	}
+
+	/// <summary>
+	/// スマホ用　クリック判定
+	/// </summary>
+	void checkTouch(){
+		if (Input.touchCount > 0) {	// タッチしている
+			// タッチの状態
+			switch (ClickLevel) {
+			case 0:
+				ClickLevel = 1;
+				break;
+			case 1:
+				ClickLevel = 2;
+				break;
+			default:
+				ClickLevel = 1;
+				break;
+			}
+			// タッチ位置を倍数に変換
+			LastClickPos = getScreenPixelToMultiple (Input.GetTouch (0).position);
+		} else {					// タッチしていない
+			ClickLevel = 0;
+		}
+	}
+
+	/// <summary>
+	/// PC用　クリック判定
+	/// </summary>
+	void checkClick(){
+		if (Input.GetKey(KeyCode.Mouse0)) {	// クリックしている
+			// クリックの状態
+			switch (ClickLevel) {
+			case 0:
+				ClickLevel = 1;
+				break;
+			case 1:
+				ClickLevel = 2;
+				break;
+			default:
+				ClickLevel = 1;
+				break;
+			}
+			// クリック位置を倍数に変換
+			LastClickPos = getScreenPixelToMultiple (Input.mousePosition);
+		} else {					// クリックしていない
+			ClickLevel = 0;
+		}
+	}
+
+	/// <summary>
+	/// マウス座標をピクセルから倍率に変換(0.0~1.0)
+	/// </summary>
+	Vector2 getScreenPixelToMultiple(Vector2 pixel){
+		Vector2 newpos = new Vector2();
+		newpos.x = pixel.x / Screen.width;
+		newpos.y = pixel.y / Screen.height;
+		Debug.Log (newpos);
+		return newpos;
+	}
+
+	/// <summary>
+	/// クリックしたかどうか
+	/// </summary>
+	public bool isClick(){
+		return ClickLevel == 1;
+	}
+
+	/// <summary>
+	/// ドラッグ中かどうか
+	/// </summary>
+	public bool isDrag(){
+		return ClickLevel > 0;
+	}
+
+	/// <summary>
+	/// マウスの位置(最後にクリックした場所)を取得 =0.0~1.0
+	/// </summary>
+	public Vector2 getMousePosition(){
+		return LastClickPos;
+	}
+}
--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -1,22 +1,19 @@
-﻿//
-// プレイヤーカメラの動き
-// ドラッグで視点移動
-//
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// カメラの向きを制御する
+/// </summary>
 public class PlayerCameraController : MonoBehaviour {
 
 	public GameObject RotX;			// 関節の読み込み
 	public GameObject RotY;
 	public GameObject RotZ;
 	public GameObject Camera;		// 子カメラ
-	public float PlayerHeight = 1.6f;	// 身長
 
 
 	// Use this for initialization
 	void Start () {
-		setViewHeight();	// 身長を反映
 	}
 	
 	// Update is called once per frame
@@ -24,10 +21,4 @@ public class PlayerCameraController : MonoBehaviour {
 		
 	}
 
-	// 身長を反映
-	void setViewHeight(){
-		Vector3 pos = RotX.transform.localPosition;
-		pos.y = PlayerHeight;
-		RotX.transform.localPosition = pos;
-	}
 }
