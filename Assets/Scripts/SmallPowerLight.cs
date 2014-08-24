b45e04295ab72e75dbfae8d9deb36870c021a5e8
eviltwo <e135764@DaikiMacBookPro.local>
2014-08-24 15:30:39 +0900

    テーブルを試設置

---
+++ Assets/Scripts/SmallPowerLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Edit時は見やすい明るさにしておき、実行時はしていした明るさにする。
+/// </summary>
+public class SmallPowerLight : MonoBehaviour {
+
+	public float Intensity = 0.05f;		// ゲーム中の明るさ
+
+	// Use this for initialization
+	void Start () {
+		light.intensity = Intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
